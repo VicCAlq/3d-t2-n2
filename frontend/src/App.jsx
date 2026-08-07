e1e4cab97ea8ebdc30392a0eaa8c836a031d7157
joao <joao.philippe@nave.org.br>
2026-08-07 14:02:23 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Exemplo from './components/Exemplo';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  const [ endereco, setEndereco ] = useState("")
+
+  async function cadastrarFeed(url) {
+    const query = new URLSearchParams('link', url)
+    await fetch(`http://localhost:3000/api/fontes/cadastrar?${query}`)
+    .then((resposta) => {
+      if (!resposta.ok) {
+        throw new Error(`Erro: ${resposta.status}`)
+      }
+      return resposta.json()
+    })
+    .then((resultado) => {
+      // Aqui você decide como usar o resultado enviado pelo servidor
+    })
+    .catch((erro) => {
+      // Vou usar "window.alert" pra exibir o erro aqui a fim de
+      // que fique mais fácil para vocês identificarem se algo der errado
+    })
+  }
+
   return (
     <View style={styles.container}>
       <ScrollView>

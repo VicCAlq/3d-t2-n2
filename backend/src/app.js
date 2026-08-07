81cfac67303ac3d7643e79f15d503ea302d7f67a
joao <joao.philippe@nave.org.br>
2026-08-07 14:00:18 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ const {
   TABELA_FONTES_NOME,
   TABELA_NOTICIAS_NOME
 } = require('./env.js')
+const { baixarFeedRSS } = require('./leitorRss.js')
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
@@ -28,7 +29,10 @@ const db = new sql.Database(
 
 db.run(
   `CREATE TABLE IF NOT EXISTS ${TABELA_FONTES_NOME} (
-    id INTEGER PRIMARY KEY AUTOINCREMENT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT,
+    link TEXT UNIQUE,
+    descricao TEXT
   )`,
   (erro) => {
     if (erro) {
@@ -41,7 +45,12 @@ db.run(
 
 db.run(
   `CREATE TABLE IF NOT EXISTS ${TABELA_NOTICIAS_NOME} (
-    id INTEGER PRIMARY KEY AUTOINCREMENT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT,
+    link TEXT UNIQUE,
+    descricao TEXT,
+    dataDePublicacao TEXT,
+    categorias TEXT
   )`,
   (erro) => {
     if (erro) {
@@ -65,14 +74,42 @@ app.get('/api/noticias/categoria/:categoria', (req, res) => {
   const categoria = req.params.categoria
 })
 
-app.get('/api/fontes/cadastrar', (req, res) => {
+app.get('/api/fontes/cadastrar', async (req, res) => {
+
   if (!req.query) {
     res.status(400).json({ error: erro.message });
     return
+  } else if (typeof(req.query.link) !== 'string') {
+    res.status(400).json({ error: `Propriedade "link" não é uma string válida` });
+  } else {
+    let url
+    try {
+      url = new URL(req.query.link)
+    } catch(err) {
+      res.status(400).json({ error: `O texto enviado não se trata de um endereço Web` });
+    }
   }
 
-  const { nome, endereco } = req.query
+  const { link } = req.query
+
+  await baixarFeedRSS(link)
+  .then(res => {
+    const fonteNoticia = [
+      res.fonte.titulo, 
+      res.fonte.link, 
+      res.fonte.descricao
+    ]
+    const noticias = res.noticias
+
+    db.run()
+
+    noticias.map(noticia => {
+      db.run(``, [] )
+    })
+  })
 })
+
+
 
 app.listen(porta, () => {
   console.log(`Servidor rodando em http://localhost:${porta}`)

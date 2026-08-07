97e624a6d1e2de4d6215b50a99b534c510958292
arii-i <ariadne.maciel@nave.org.br>
2026-08-07 16:35:47 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 import Exemplo from './components/Exemplo';
 
 export default function App() {
   return (
     <View style={styles.principal}>
 
-      <View style={styles.wanda}>
+
+
 
         
         <View style={styles.topo}> 
 
-            <Pressable> 
+            <Pressable style={styles.botao}> 
 
             <Text style={styles.texto}>Categoria V</Text>
 
           </Pressable>
 
-          <Pressable>
+          <Pressable style={styles.botao}
+          onPress={() => setFiltro("fonte")}>
 
             <Text style={styles.texto}>Fonte V</Text>
 
           </Pressable>
 
-
-        </View>
-      
-
-
       </View>
+
+      <View>
+
+        
+      
+      </View>
+
 
     </View>
   );
@@ -39,17 +43,7 @@ const styles = StyleSheet.create({
   principal: {
     flex: 1,
     backgroundColor: "#eec",
-    flexDirection: 'row',
     height: '100%',
-    width: '100%'
-  },
-
-  wanda: {
-    backgroundColor: "rgba(235, 235, 9, 1)",
-    flexDirection: 'row',
-    typeSize: 'large',
-    justifyContent: 'center',
-    height: '15%',
     width: '100%'
   },
 
@@ -59,11 +53,20 @@ const styles = StyleSheet.create({
   },
 
     topo: {
+    width: '100%',
+     height: '10%',
     backgroundColor: "yellow",
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    gap: '50%',
+    gap: 20,
+  },
+
+   botao: {
+    paddingHorizontal: 25,
+    paddingVertical: 10,
+    borderRadius: 8,
+    backgroundColor: "#2f00ffff",
   },
 
 

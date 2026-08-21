4ca08193a28f83ebe892b37dd5a09d72a4affa82
Azulejo-barroco <enzo.almeida@nave.org.br>
2026-08-21 13:48:19 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,379 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import Exemplo from './components/Exemplo';
+import { useState,  useEffect } from 'react';
+import { StyleSheet, Text, View, Pressable, TextInput} from 'react-native';
 
 export default function App() {
+
+  const [tabelaNoticias, setTabelaNoticias] = useState([]);
+  const [endereco, setEndereco] = useState("");
+  const [fonte, setFonte] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [filtro, setFiltro] = useState("");
+  const [erro, setErro] = useState("");
+
+    const categorias = [ "politica",
+    "esportes",
+    "tecnologia",
+    "economia"
+  ];
+
+
+  async function cadastrarFeed(url) {
+    const query = new URLSearchParams({link: url})
+    await fetch(`http://localhost:3000/api/fontes/cadastrar?${query}`)
+    .then((resposta) => {
+      if (!resposta.ok) {
+        throw new Error(`Erro: ${resposta.status}`)
+      }
+      return resposta.json()
+    })
+    .then((resultado) => {
+
+        console.log(resultado);
+        setEndereco("");
+        setErro("");
+        carregarNoticias();
+    })
+    .catch((erro) => {
+
+        console.log(erro);
+        setErro(
+          "Não foi possível reconhecer a fonte, verifique e tente novamente"
+        );
+
+    })
+  }
+
+  async function carregarNoticias(fonte) {
+    let rota = "http://localhost:3000/api/noticias"
+    if (fonte) {
+      rota = rota + "/fonte/" + fonte
+    }
+    await fetch(rota)
+    .then((resposta) => {
+      if (!resposta.ok) {
+        throw new Error(`Erro: ${resposta.status}`)
+      }
+      return resposta.json()
+    })
+    .then((resultado) => {
+
+        setTabelaNoticias(resultado);
+        setErro("");
+
+    })
+    .catch((erro) => {
+              console.log(erro);
+
+        setErro(
+          "Não foi possível carregar as notícias, tente novamente."
+        );
+    })
+  }
+
+  async function filtrarPorFonte(nomeFonte) {
+    const rota = "http://localhost:3000/api/noticias/fonte" + nomeFonte;
+
+    await fetch(rota)
+    .then((resposta) => {
+      if (!resposta.ok) {
+        throw new Error(`Erro: ${resposta.status}`)
+      }
+      return resposta.json()
+    })
+    .then((resultado) => {
+
+        setTabelaNoticias(resultado);
+        setFonte(nomeFonte);
+        setErro("");
+
+    })
+    .catch((erro) => {
+
+        console.log(erro);
+        setErro(
+          "Não foi possível encontrar fonte"
+        );
+
+    })
+  }
+
+async function filtrarPorCategoria(nomeCategoria) {
+    const rota = "http://localhost:3000/api/noticias/categoria" + nomeCategoria;
+
+    await fetch(rota)
+    .then((resposta) => {
+      if (!resposta.ok) {
+        throw new Error(`Erro: ${resposta.status}`)
+      }
+      return resposta.json()
+    })
+    .then((resultado) => {
+
+        setTabelaNoticias(resultado);
+        setCategoria(nomeCategoria);
+        setErro("");
+
+
+    })
+    .catch((erro) => {
+
+        console.log(erro);
+        setErro(
+          "Não foi possível encontrar categoria."
+        );
+
+    })
+  }
+
+  useEffect(() => {
+  carregarNoticias();
+},[]);
+
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        <Text>Comece aqui seu projeto Mobile</Text>
-        <Exemplo>Este é um componente de exemplo</Exemplo>
-      </ScrollView>
+    <View style={styles.principal}>
+
+      <View style={styles.topo}>
+  <Text style={styles.titulo}>
+    Site foda
+  </Text>
+</View>
+
+<View style={styles.formulario}>
+
+  <Text style={styles.subtitulo}>
+    Cadastrar nova fonte
+  </Text>
+
+  <TextInput
+    style={styles.input}
+    value={endereco}
+    onChangeText={setEndereco}
+    placeholder="Digite o link da fonte"
+  />
+
+  <Pressable
+    style={styles.botaoCadastrar}
+    onPress={() => cadastrarFeed(endereco)}
+  >
+    <Text style={styles.textoBotao}>
+      Cadastrar fonte
+    </Text>
+  </Pressable>
+
+</View>
+     
+      <View style={styles.topoFiltros}>
+          <Pressable style={styles.botao}
+            onPress={() => setFiltro("categoria")}>
+            <Text style={styles.texto}>Categoria ▼</Text>
+          </Pressable>
+        
+          <Pressable style={styles.botao}
+          onPress={() => setFiltro("fonte")}>
+            <Text style={styles.texto}>Fonte ▼</Text>
+          </Pressable>
+      </View>
+
+{filtro === "categoria" && (<View style={styles.menuFiltro}>
+
+    <Text style={styles.subtitulo}>
+      Escolha uma categoria:
+    </Text>
+
+    {categorias.map((categoria) => (
+      <Pressable key={categoria} style={styles.opcao}
+        onPress={() => filtrarPorCategoria(categoria)
+        }>
+
+        <Text>
+          {categoria}
+        </Text>
+      </Pressable>
+
+    ))}
+
+    </View>)}
+
+  {filtro === "fonte" && (<View style={styles.menuFiltro}>
+
+          <Text style={styles.subtitulo}>
+            Filtrar por fonte:
+          </Text>
+
+
+          <TextInput style={styles.input} value={fonte} onChangeText={setFonte}/>
+
+
+          <Pressable style={styles.botaoPesquisar}
+           onPress={() => filtrarPorFonte(fonte)}>
+
+            <Text style={styles.textoBotao}>
+              Pesquisar
+            </Text>
+
+          </Pressable>
+
+        </View>
+
+      )}
+
+
+<View style={styles.lista}>
+         {tabelaNoticias.map((noticia) => (
+
+            <View key={noticia.id} style={styles.noticia}>
+
+              <Text style={styles.tituloNoticia}>
+                {noticia.titulo}
+              </Text>
+
+
+              <Text style={styles.descricao}>
+                {noticia.descricao}
+              </Text>
+
+
+              <Text style={styles.informacao}>
+                Fonte: {noticia.fonte}
+              </Text>
+
+
+              <Text style={styles.informacao}>
+                Categoria: {noticia.categoria}
+              </Text>
+
+              </View>))}
+
+</View>
+
+
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
+  principal: {
     flex: 1,
     backgroundColor: "#eec",
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: '100%',
+    width: '100%'
   },
-  title: {
-    color: "#101015"
-  }
+
+    topo: {
+    width: "100%",
+    height: "10%",
+    backgroundColor: "yellow",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+    titulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  formulario: {
+    padding: 15,
+    backgroundColor: "white",
+  },
+
+  subtitulo: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  botaoCadastrar: {
+    paddingVertical: 10,
+    borderRadius: 8,
+    backgroundColor: "#008000",
+    alignItems: "center",
+  },
+
+  topoFiltros: {
+    width: "100%",
+    padding: 10,
+    backgroundColor: "yellow",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+
+  texto: {
+    color: "white",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+
+   botao: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 8,
+    backgroundColor: "#2f00ff",
+  },
+
+   textoBotao: {
+
+    color: "white",
+    fontSize: 15
+  },
+
+input: {
+  borderWidth: 1,
+  borderColor: "#999",
+  borderRadius: 8,
+  paddingHorizontal: 12,
+  paddingVertical: 10,
+  marginBottom: 10,
+},
+
+menuFiltro: {
+  backgroundColor: "white",
+  padding: 15,
+},
+
+opcao: {
+  padding: 10,
+  borderBottomWidth: 1,
+  borderBottomColor: "#ccc",
+},
+
+botaoPesquisar: {
+  paddingVertical: 10,
+  borderRadius: 8,
+  backgroundColor: "#2f00ff",
+  alignItems: "center",
+},
+
+lista: {
+  padding: 15,
+},
+
+noticia: {
+  backgroundColor: "white",
+  padding: 15,
+  marginBottom: 10,
+  borderRadius: 8,
+},
+
+tituloNoticia: {
+  fontSize: 18,
+  fontWeight: "bold",
+  marginBottom: 5,
+},
+
+descricao: {
+  fontSize: 14,
+  marginBottom: 8,
+},
+
+informacao: {
+  fontSize: 13,
+  marginTop: 3,
+}
+
 });

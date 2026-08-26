329ef58f0075304ff292db38a4e146bffe73a4ad
SenaPaiva <mariaeduarda.sena@nave.org.br>
2026-08-26 04:42:19 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import Exemplo from './components/Exemplo';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import Exemplo from "./components/Exemplo";
+import { useEffect, useState } from "react";
+
+const API_URL = "http://localhost:3451";
 
 export default function App() {
+  const [noticias, setNoticias] = useState([]);
+  const [status, setStatus] = useState("Testando conexão...");
+
+  useEffect(() => {
+    fetch(`${API_URL}/`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Erro HTTP: ${res.status}`);
+        }
+
+        return res.json();
+      })
+      .then((data) => {
+        console.log("BACKEND RESPONDEU:", data);
+
+        setStatus("✅ Backend conectado!");
+      })
+      .catch((erro) => {
+        console.log("ERRO DE CONEXÃO:", erro);
+
+        setStatus("❌ Backend desconectado!");
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
       <ScrollView>
-        <Text>Comece aqui seu projeto Mobile</Text>
-        <Exemplo>Este é um componente de exemplo</Exemplo>
+
+        <Text style={styles.title}>
+          Agregador de notícias
+        </Text>
+
+        <Text style={styles.status}>
+          {status}
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Cadastrar nova fonte de notícias
+        </Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Cole aqui o link do feed RSS"
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>
+            Enviar
+          </Text>
+        </TouchableOpacity>
+
+        <Text style={styles.sectionTitle}>
+          Filtrar notícias
+        </Text>
+
+        <Text style={styles.label}>
+          Categoria
+        </Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Todas"
+        />
+
+        <Text style={styles.label}>
+          Fonte
+        </Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="SES - Notícias"
+        />
+
       </ScrollView>
     </View>
   );
@@ -16,11 +94,54 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#eec",
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
+
   title: {
-    color: "#101015"
-  }
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 15,
+  },
+
+  status: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+
+  subtitle: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: "#999",
+    padding: 10,
+    marginBottom: 15,
+  },
+
+  button: {
+    backgroundColor: "#3498db",
+    padding: 15,
+    alignItems: "center",
+    marginBottom: 25,
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+
+  sectionTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 15,
+  },
+
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
 });
+
+//por motivos q eu n conheço, eu n to  conseguindo conectar o back com o front (╥﹏╥)
